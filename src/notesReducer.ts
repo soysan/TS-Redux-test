0c4d83c098a9ca755ca6d67780b92dc755b63981
Masayuki <soy2.san@gmail.com>
2021-04-07 16:18:08 +0900

    test ofTS with Redux

---
+++ src/notesReducer.ts
@@ -0,0 +1,22 @@
+import { Action } from "./action";
+
+export interface NotesState {
+    notes: string[];
+}
+
+const initialState = {
+    notes: []
+}
+
+export const notesReducer = (state: NotesState = initialState, action: Action) => {
+    switch (action.type) {
+        case "ADD_NOTE":
+            return {
+                ...state,
+                notes: [...state.notes, action.payload]
+            }
+
+        default:
+            return state;
+    }
+}
